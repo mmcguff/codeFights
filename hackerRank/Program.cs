0bb82068411489c42fc9cc0437ed4de1a8408347
Matthew McGuff <matthewmcguff85@gmail.com>
2018-01-02 15:59:12 -0700

    Initial commit for first duplicate with code fights.  All but one test passes.  Not really sure why yet.

---
+++ hackerRank/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hackerRank
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] test1 = { 2, 3, 3, 1, 5, 2 };
+            int ans = firstDuplicate(test1);
+            Console.WriteLine("The duplicate is " + ans + ".");
+
+            Console.ReadKey();
+        }
+
+        static int firstDuplicate(int[] a)
+        {
+            Object obj = new Object();
+
+            for (int i = 0; i < a.Length; i++ )
+            {
+                if(obj.Equals(a[i]))
+                {
+                    return a[i];
+                }
+                else
+                {
+                    obj = a[i]; 
+                }
+            }
+
+
+                return -1;
+
+        
+        }
+    }
+}
