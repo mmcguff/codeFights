5b524a83cede73116b64f14ac0fd22db1fc21fae
Matthew McGuff <matthewmcguff85@gmail.com>
2018-01-04 09:05:07 -0700

    adding new project

---
+++ rotateImage/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rotateImage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[]
+                {
+                    {1, 2, 3}
+                   ,{4, 5, 6}
+                   ,{7, 8, 9}
+                };
+
+        //need to figure out how to declare and use multiple dimensinoal array syntax. 
+            Console.ReadKey();
+        }
+
+        int[][] rotateImage(int[][] a)
+        {
+            return a;
+        }
+    }
+}
